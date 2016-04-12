18c06d11ba396b796736c5d91aa9e572d5409033
Vitor Barros <vitormonteirodebarros@gmail.com>
2016-04-12 16:51:40 -0300

    adicionando o método de validar os campos obrigatórios

--- src/VMBFormFieldsValidator/Form/FormFieldsRequired.php
+++ src/VMBFormFieldsValidator/Form/FormFieldsRequired.php
@@ -1,22 +1,49 @@
 <?php
 namespace VMBFormFieldsValidator\Form;
 
+use Zend\I18n\Translator\Translator;
 use Zend\InputFilter\InputFilter;
 
 class FormFieldsRequired extends InputFilter
 {
 
     private $fields = array();
+    private $locale;
+    private $translator;
 
-    public function __construct(array $fields)
+    public function __construct(array $fields, $locale)
     {
         $this->fields = $fields;
+        $this->locale = $locale;
+        $this->translator = new Translator();
+        $this->fieldsRequired();
     }
 
-    public function fieldsRequired()
+    private function fieldsRequired()
     {
-        if(isset($this->fields['fieldsRequired'])){
-            
+        if (isset($this->fields['fieldsRequired'])) {
+            foreach ($this->fields['fieldsRequired'] as $name => $label) {
+
+                $filter = array(
+                    'name' => $name,
+                    'require' => true,
+                    'filters' => array(
+                        array('name' => 'StripTags'),
+                        array('name' => 'StringTrim')
+                    ),
+                    'validators' => array(
+                        array(
+                            'name' => 'notEmpty',
+                            'options' => array(
+                                'message' => $this->translator->translate("The field <b>{$label}</b> cannot be null", "default", $this->locale)
+                            )
+                        )
+                    )
+                );
+
+                $this->add($filter);
+
+            }
         }
         throw new \Exception("index 'fieldsRequired' is missing");
     }
