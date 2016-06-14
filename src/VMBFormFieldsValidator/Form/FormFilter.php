6ed098fb1af8ab972066411e22dc81921fb94ee4
Vitor Barros <vitormonteirodebarros@gmail.com>
2016-06-14 12:05:36 -0300

    pequena modificação

--- src/VMBFormFieldsValidator/Form/FormFilter.php
+++ src/VMBFormFieldsValidator/Form/FormFilter.php
@@ -3,7 +3,6 @@ namespace VMBFormFieldsValidator\Form;
 
 class FormFilter extends AbstractFormValidator
 {
-
     public function __construct(array $fields)
     {
         parent::__construct($fields);
